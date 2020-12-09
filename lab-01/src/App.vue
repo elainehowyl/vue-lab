6546359e355b4836db45deac74cd58edbb7db339
elainehowyl <elaine_0110@hotmail.com>
2020-12-09 07:03:24 +0000

    first vue run

---
+++ lab-01/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <messageBox></messageBox>
+    <h1>Goodbye World</h1>
+  </div>
+</template>
+
+<script>
+
+import messageBox from "./components/messageBox"
+
+export default {
+  name: 'App',
+  components: {
+     messageBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lab-01/src/components/messageBox.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- inside template, you can only have one parent element -->
+ <div>
+     <div>Welcome</div>
+     <div>{{message}}</div>
+ </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            'message':'very hello world'
+        }
+    }
+}
+</script>
+<style scoped>
+div{
+    background-color:pink;
+}
+</style>
